f2fe3d565749d6dd03c0e1f83238a0772cb5202a
NguyenLeHoai <www.dragonwatervnvn@gmail.com>
2024-03-20 23:19:31 +0700

    Hoai1221

---
+++ Controllers/UserAccountsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SWD_Project.Models;
+
+namespace SWD_Project.Controllers
+{
+    public class UserAccountsController : Controller
+    {
+        private readonly Swd392Context _context;
+
+        public UserAccountsController(Swd392Context context)
+        {
+            _context = context;
+        }
+
+        // GET: UserAccounts
+        public async Task<IActionResult> Index(string searchString)
+        {
+            if (_context.UserAccounts == null)
+            {
+                return Problem("Entity set 'Swd392Context.UserAccounts' is null.");
+            }
+
+            var UserName = from t in _context.UserAccounts
+                        select t;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                UserName = UserName.Where(s => s.Username!.Contains(searchString));
+            }
+
+            return View(await UserName.ToListAsync());
+        }
+
+
+        // GET: UserAccounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UserAccounts == null)
+            {
+                return NotFound();
+            }
+
+            var userAccount = await _context.UserAccounts
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (userAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(userAccount);
+        }
+
+        // GET: UserAccounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,Username,Password,Role,Status")] UserAccount userAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userAccount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userAccount);
+        }
+
+        // GET: UserAccounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UserAccounts == null)
+            {
+                return NotFound();
+            }
+
+            var userAccount = await _context.UserAccounts.FindAsync(id);
+            if (userAccount == null)
+            {
+                return NotFound();
+            }
+            return View(userAccount);
+        }
+
+        // POST: UserAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,Username,Password,Role,Status")] UserAccount userAccount)
+        {
+            if (id != userAccount.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserAccountExists(userAccount.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userAccount);
+        }
+
+        // GET: UserAccounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UserAccounts == null)
+            {
+                return NotFound();
+            }
+
+            var userAccount = await _context.UserAccounts
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (userAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(userAccount);
+        }
+
+        // POST: UserAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UserAccounts == null)
+            {
+                return Problem("Entity set 'Swd392Context.UserAccounts'  is null.");
+            }
+            var userAccount = await _context.UserAccounts.FindAsync(id);
+            if (userAccount != null)
+            {
+                _context.UserAccounts.Remove(userAccount);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserAccountExists(int id)
+        {
+          return (_context.UserAccounts?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+    }
+}
