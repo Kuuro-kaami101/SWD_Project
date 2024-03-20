848f8574451396fe612c10bc262d9b9779814765
leon <vothanhlong15052003@gmail.com>
2024-03-20 23:25:50 +0700

    qwe

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SWD_Project.Models;
+using System.Security.Claims;
+
+namespace SWD_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly Swd392Context _context;
+
+        public LoginController(Swd392Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var user = _context.UserAccounts.Include(x => x.Role).FirstOrDefault(x => x.Username == username && x.Password == password);
+            if (user == null)
+            {
+                return View();
+            }
+            List<Claim> claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role),
+                    new Claim(ClaimTypes.Name, user.Username),
+                };
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+
+                AllowRefresh = true,
+                IsPersistent = true,
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), properties);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
